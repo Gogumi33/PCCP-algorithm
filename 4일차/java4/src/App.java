e214ab5f6c1c56b256b399234586e7a6becfa6e2
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-05-13 18:23:11 +0900

    string java

--- 4일차/java4/src/App.java
+++ 4일차/java4/src/App.java
@@ -1,6 +1,14 @@
+import java.io.*;
+import java.util.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("test");
+    public int solution(String s) {
+        String[] num= {"0","1","2","3","4","5","6","7","8","9"};
+        String[] word= {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for( int i=0; i<10; i++ ){
+            s = s.replace(word[i], num[i]);
+        }
+        return Integer.parseInt(s);
     }
 }
